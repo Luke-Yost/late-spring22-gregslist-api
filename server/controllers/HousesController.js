3f7dbde4ea9ea58dd6c37fed065bfd7f2228ef5e
Luke Yost <lukexyost@gmail.com>
2022-06-07 16:39:49 -0600

    started housescontroller

---
+++ server/controllers/HousesController.js
@@ -0,0 +1,31 @@
+import BaseController from "../utils/BaseController.js";
+import { logger } from "../utils/Logger.js";
+
+
+
+export class HousesController extends BaseController {
+
+  constructor(){
+    super('api/houses')
+    this.router
+
+  }
+
+  async getAllHouses(req, res, next){
+    try {
+      logger.log("[QUERY]", req.query)
+      const houses = await housesService.getAllHouses(req.query)
+      return res.send(houses)
+    }
+    catch(error){
+      next(error)
+    }
+  }
+
+  // async makeHouse(req, res, next){
+  //   try {
+      
+  //   }
+  // }
+
+}
